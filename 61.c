d32882a0ea2ffa0e5470cd75f7bf80689abd8179
harib11112006-stack <harib11112006@gmail.com>
2026-09-02 15:51:47 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+
+bool uniformArray(int* nums1, int nums1Size) {
+    // It is mathematically always possible to construct 
+    // the array under the given parity conditions.
+    return true;
+}
